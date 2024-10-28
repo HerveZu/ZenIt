6789c4c63c9175a7c9298e1e37b3137e8b69f181
Hervé Zucchinetti <herve.zucchinetti@gmail.com>
2024-10-28 20:34:19 +0100

    Rename yolov9c to yolov8

--- model-training/notebook/config.py
+++ model-training/notebook/config.py
@@ -3,6 +3,6 @@ import os
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-MODEL_PATH = os.path.join(dir_path, "../model/yolov9c/weights/best.pt")
+MODEL_PATH = os.path.join(dir_path, "../model/yolov8/weights/best.pt")
 DATA_RECONFIG_PATH = os.path.join(dir_path, "../config.yaml")
 YOLO_DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
